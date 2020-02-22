045f0e8497f7ab2a82ec29bcbad79b7afd024e4b
carlomicieli <carlo.micieli@gmail.com>
2020-02-22 10:07:42 +0100

    Clean up use cases registration for DI

--- Src/Web/DependencyInjection/ApplicationExtensions.cs
+++ Src/Web/DependencyInjection/ApplicationExtensions.cs
@@ -2,6 +2,10 @@
 using TreniniDotNet.Application.UseCases;
 using TreniniDotNet.Domain.Catalog.Brands;
 using TreniniDotNet.Domain.Catalog.Scales;
+using TreniniDotNet.Domain.Catalog.Railways;
+
+using CatalogUseCases = TreniniDotNet.Application.UseCases.Catalog;
+using CatalogBoundaries = TreniniDotNet.Application.Boundaries.Catalog;
 
 namespace TreniniDotNet.Web.DependencyInjection
 {
@@ -9,11 +13,12 @@ namespace TreniniDotNet.Web.DependencyInjection
     {
         public static IServiceCollection AddUseCases(this IServiceCollection services)
         {
-            services.AddScoped<TreniniDotNet.Application.Boundaries.Catalog.CreateBrand.ICreateBrandUseCase, TreniniDotNet.Application.UseCases.Catalog.CreateBrand>();
-            services.AddScoped<TreniniDotNet.Application.Boundaries.Catalog.GetBrandBySlug.IGetBrandBySlugUseCase, TreniniDotNet.Application.UseCases.Catalog.GetBrandBySlug>();
-            services.AddScoped<TreniniDotNet.Application.Boundaries.Catalog.CreateScale.ICreateScaleUseCase, TreniniDotNet.Application.UseCases.Catalog.CreateScale>();
+            services.AddScoped<CatalogBoundaries.CreateBrand.ICreateBrandUseCase, CatalogUseCases.CreateBrand>();
+            services.AddScoped<CatalogBoundaries.GetBrandBySlug.IGetBrandBySlugUseCase, CatalogUseCases.GetBrandBySlug>();
+            services.AddScoped<CatalogBoundaries.CreateScale.ICreateScaleUseCase, CatalogUseCases.CreateScale>();
 
             services.AddScoped<BrandService>();
+            services.AddScoped<RailwayService>();
             services.AddScoped<ScaleService>();
 
             services.AddSingleton(typeof(IUseCaseInputValidator<>), typeof(UseCaseInputValidator<>));
