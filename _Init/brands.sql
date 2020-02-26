f5ffb7ef754ff61e901ba1beab03a1361e9c5ce1
carlomicieli <carlo.micieli@gmail.com>
2020-02-26 07:27:53 +0100

    Init script for brands

---
+++ _Init/brands.sql
@@ -0,0 +1,35 @@
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('9ed9f089-2053-4a39-b669-a6d603080402', 'acme', 'ACME', 'http://www.acmetreni.com', 'mail@acmetreni.com', 'Associazione Costruzioni Modellistiche Esatte', 'Industrial', null, 1, '2020-02-26');
+	
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('4b7a619b-65cc-41f5-a003-450537c85dea', 'roco', 'Roco', 'http://www.roco.cc', null, 'Modelleisenbahn GmbH', 'Industrial', null, 1, '2020-02-26');
+	
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('ff9f5055-8ae7-4d58-a68f-0cee3adb6656', 'bemo', 'Bemo', 'https://www.bemo-modellbahn.de', 'mail@bemo-modellbahn.de', 'BEMO Modelleisenbahnen GmbH u. Co KG', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('c37f8ac1-991b-422c-b273-5c02efe2087e', 'brawa', 'Brawa', 'https://www.brawa.de', null, 'BRAWA Artur Braun Modellspielwarenfabrik GmbH & Co. KG', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('66fa5a39-7e47-471f-9f92-d2bb01258c31', 'maerklin', 'Märklin', 'https://www.maerklin.de', null, 'Gebr. Märklin & Cie. GmbH', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('2a916d99-953f-44d6-8115-7e72ca22b081', 'fleischmann', 'Fleischmann', 'https://www.fleischmann.de', null, 'Modelleisenbahn München GmbH', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('528128c3-7316-44c6-957c-761cecf0920c', 'vitrains', 'ViTrains', 'http://www.vitrains.it', null, 'ViTrains', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('9391bd6a-d21f-40c8-9600-84ea1e3eb2f0', 'ls-models', 'Ls Models', 'https://www.lsmodels.com', 'info@lsmodels.com', 'L.S. MODELS EXCLUSIVE SPRL', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('016f97ff-b9aa-4818-b784-76096185db85', 'piko', 'Piko', 'https://www.piko.de', 'hotline@piko.de', 'PIKO Spielwaren GmbH', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('4fea60f1-78c8-4302-b781-076fa409fe30', 'liliput', 'Liliput', 'https://www.liliput.de', 'service@liliput.de', 'Bachmann Europe Plc', 'Industrial', null, 1, '2020-02-26');
+
+INSERT INTO public."Brands"("BrandId", "Slug", "Name", "WebsiteUrl", "EmailAddress", "CompanyName", "BrandKind", "Active", "Version", "CreatedAt")
+VALUES ('9d044507-0d00-46aa-8798-ea3c77277c28', 'tillig', 'Tillig', 'https://www.tillig.com', 'info@tillig.com', 'TILLIG Modellbahnen GmbH', 'Industrial', null, 1, '2020-02-26');
+
+
+
