814e1b6cfc96d7105d141edefd3a307b48f9fbf2
carlomicieli <carlo.micieli@gmail.com>
2020-02-26 19:52:54 +0100

    Init scripts

---
+++ _Init/railways.sql
@@ -0,0 +1,79 @@
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('3ee3f78c-5d6a-47f8-9df5-ae1e99b6148a', 'ÖBB', 'obb', 'Österreichische Bundesbahnen', 'AT', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('452ecf0f-0999-443d-a333-7afc23542d38', 'SNCB', 'sncb', 'Société Nationale des Chemins de fer belges', 'BE', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('b748e515-ff47-4a1e-a650-deb4e72ba77f', 'SŽDC', 'szdc', 'Správa železniční dopravní cesty, s.o.', 'CZ', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('22d13976-b42e-45e8-9e50-4c64c9664014', 'DSB', 'dsb', 'Danske Statsbaner', 'DK', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('5cbccdd5-826d-4dc8-a0ff-d80572d43ac5', 'SNCF', 'sncf', 'Société Nationale des Chemins de fer Français', 'FR', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('f12a3c5b-21f0-4d96-baf0-7bbf67e85e93', 'DB', 'db', 'Die Bahn AG', 'DE', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('24510242-38f8-4da7-b769-9163ba745f30', 'DR', 'dr', 'Deutsche Reichsbahn', 'DE', 'Inactive', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('13d47808-ecb2-4a44-b38d-d7b1b6e2eefb', 'DRB', 'drb', 'Deutsche Reichsbahn-Gesellschaft', 'DE', 'Inactive', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('0d7ae9ae-6075-4625-a1c2-0f21152ae21c', 'FS', 'fs', 'Ferrovie dello Stato', 'IT', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('459481c6-ca97-4897-a6fa-205daf0405cf', 'FNM', 'fnm', 'Ferrovie Nord Milano', 'IT', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('8a544884-5953-42f8-8d37-af661532596d', 'Trenord', 'Trenord', 'Trenord', 'IT', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('a1a8b2ef-1b2e-4d66-b8bf-b93ebf3d1871', 'CFL', 'cfl', 'Chemins de Fer Luxembourgeois', 'LU', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('a1423324-d37e-4529-8e36-5c4ac9316191', 'NS', 'ns', 'Nederlandse Spoorwegen', 'NL', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('12cd1791-bb51-476d-bdce-8c452284a31a', 'Vy', 'vy', 'Vygruppen', 'NO', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('f4c5a9d2-92a8-48b6-87ac-728fca41885d', 'Cp', 'cp', 'Comboios de Portugal', 'PT', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('125cea2c-9dbd-4b7e-916b-8ee78e86b4b6', 'RENFE', 'renfe', 'Renfe Operadora', 'ES', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('b4cf4d31-86d4-4f80-a34f-970cf91c4732', 'BLS', 'bls', 'BLS AG', 'CH', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('fa307786-00a9-4257-9274-76f7a0c06fab', 'RhB', 'rhb', 'Rhätische Bahn / Viafier retica', 'CH', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('1c44e65f-bb53-4f14-a368-23daa5bfee05', 'SBB', 'sbb', 'Schweizerische Bundesbahnen', 'CH', 'Active', null, null, 1, '2020-02-26');
+
+INSERT INTO public."Railways"
+    ("RailwayId", "Name", "Slug", "CompanyName", "Country", "Status", "OperatingSince", "OperatingUntil", "Version", "CreatedAt")
+VALUES('5410de14-3f6b-4ccc-9705-68b12442992c', 'MGB', 'mgb', 'Matterhorn-Gotthard-Bahn', 'CH', 'Active', null, null, 1, '2020-02-26');
---
+++ _Init/scales.sql
@@ -0,0 +1,39 @@
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('75a3dc93-9f10-4cbf-a70f-ae57b784b038', 'H0', 'h0', 16.5, 87, 'Standard', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('9b204db4-ed7c-4eae-aec0-d2ed275e4660', 'n', 'n', 9, 160, 'Standard', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('9fbde41c-be19-4208-8e69-85e1e12b2660', 'Nm', 'nm', 6.5, 160, 'Narrow', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('a926194b-220a-4a9c-8e55-89ebf52c65e0', 'Ne', 'ne', 4.5, 160, 'Narrow', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('fb7ab3fc-5f15-4e2c-a8d3-7ef2e615dae8', '1', '1', 45, 32, 'Standard', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('efc1fdb5-93aa-4a52-bbce-5ab67e92980c', '0', '0', 32, 43.5, 'Standard', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('02790f5e-8edc-43f6-8ac1-4c906805d9ba', 'Z', 'z', 6.5, 220, 'Standard', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('374f5bb7-e7d1-4995-aa34-072b6b6500f9', 'TT', 'tt', 12, 120, 'Standard', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('0dd13f9d-d730-41bb-b4e9-33218ea14fbc', 'H0m', 'h0m', 12, 87, 'Narrow', '2020-02-26', 1);
+
+INSERT INTO public."Scales"
+    ("ScaleId", "Name", "Slug", "Gauge", "Ratio", "TrackGauge", "CreatedAt", "Version")
+VALUES('b5f2f033-a947-4b86-9d9e-52d7c1903ce0', 'H0e', 'h0e', 9, 87, 'Narrow', '2020-02-26', 1);
