43dee6f900154b0c497a8df4338b28262919d084
Carlo Micieli <carlo.micieli@gmail.com>
2020-04-12 14:57:50 +0200

    Added missing fields to RailwayViews

--- Src/Web/ViewModels/V1/Catalog/RailwayView.cs
+++ Src/Web/ViewModels/V1/Catalog/RailwayView.cs
@@ -14,9 +14,25 @@ namespace TreniniDotNet.Web.ViewModels.V1.Catalog
             this.Name = railway.Name;
             this.CompanyName = railway.CompanyName;
             this.Country = railway.Country.ToString();
-            this.Status = railway.PeriodOfActivity.RailwayStatus.ToString();
-            this.OperatingSince = railway.PeriodOfActivity.OperatingSince;
-            this.OperatingUntil = railway.PeriodOfActivity.OperatingUntil;
+            this.WebsiteUrl = railway.WebsiteUrl?.ToString();
+            this.Headquarters = railway.Headquarters;
+
+            this.PeriodOfActivity = new PeriodOfActivityView
+            {
+                Status = railway.PeriodOfActivity.RailwayStatus.ToString(),
+                OperatingSince = railway.PeriodOfActivity.OperatingSince,
+                OperatingUntil = railway.PeriodOfActivity.OperatingUntil
+            };
+
+            if (!(railway.TotalLength is null))
+            {
+                this.TotalLength = new TotalLengthView(railway.TotalLength);
+            }
+
+            if (!(railway.TrackGauge is null))
+            {
+                this.TrackGauge = new RailwayGaugeView(railway.TrackGauge);
+            }
         }
 
         [JsonPropertyName("_links")]
@@ -30,10 +46,47 @@ namespace TreniniDotNet.Web.ViewModels.V1.Catalog
 
         public string? Country { get; set; }
 
-        public string? Status { get; set; }
+        public PeriodOfActivityView? PeriodOfActivity { get; set; }
 
-        public DateTime? OperatingUntil { get; set; }
+        public TotalLengthView? TotalLength { get; set; }
 
-        public DateTime? OperatingSince { get; set; }
+        public RailwayGaugeView? TrackGauge { get; set; }
+
+        public string? Headquarters { get; set; }
+
+        public string? WebsiteUrl { get; set; }
+    }
+
+    public class PeriodOfActivityView
+    {
+        public string? Status { set; get; }
+        public DateTime? OperatingSince { set; get; }
+        public DateTime? OperatingUntil { set; get; }
+    }
+
+    public class TotalLengthView
+    {
+        public TotalLengthView(RailwayLength railwayLength)
+        {
+            Miles = decimal.Round(railwayLength.Miles.Value, 0);
+            Kilometers = decimal.Round(railwayLength.Kilometers.Value, 0);
+        }
+
+        public decimal? Miles { get; }
+        public decimal? Kilometers { get; }
+    }
+
+    public class RailwayGaugeView
+    {
+        public RailwayGaugeView(RailwayGauge rg)
+        {
+            Millimeters = decimal.Round(rg.Millimeters.Value, 1);
+            Inches = decimal.Round(rg.Inches.Value, 1);
+            Gauge = rg.TrackGauge.ToString();
+        }
+
+        public decimal? Millimeters { get; }
+        public decimal? Inches { get; }
+        public string? Gauge { get; }
     }
 }
---
+++ Tests/Web.UnitTests/ViewModels/V1/Catalog/RailwayViewTests.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using FluentAssertions;
+using TreniniDotNet.TestHelpers.SeedData.Catalog;
+using TreniniDotNet.Domain.Catalog.ValueObjects;
+using System;
+using TreniniDotNet.Common;
+using TreniniDotNet.Domain.Catalog.Railways;
+using TreniniDotNet.Web.ViewModels.Links;
+
+namespace TreniniDotNet.Web.ViewModels.V1.Catalog
+{
+    public class RailwayViewTests
+    {
+        [Fact]
+        public void RailwayView_ShouldRenderARailway_WithBasicInfo()
+        {
+            var expectedId = new Guid("e8d33cd3-f36b-4622-90d1-76b450e0f313");
+            var view = RailwayViewWith(id: expectedId);
+
+            view.Should().NotBeNull();
+            view.Id.Should().Be(expectedId);
+            view.Name.Should().Be("FS");
+            view.Country.Should().Be("Italy");
+            view.CompanyName.Should().Be("Ferrovie dello stato");
+            view.WebsiteUrl.Should().Be("http://www.trenitalia.com/");
+            view.Headquarters.Should().Be("Roma");
+
+            view.PeriodOfActivity.Should().NotBeNull();
+            view.PeriodOfActivity.Status.Should().Be("Active");
+            view.PeriodOfActivity.OperatingSince.Should().BeNull();
+            view.PeriodOfActivity.OperatingUntil.Should().BeNull();
+            view.TrackGauge.Should().BeNull();
+            view.TotalLength.Should().BeNull();
+        }
+
+        [Fact]
+        public void RailwayView_ShouldRenderARailway_WithTotalLength()
+        {
+            var view = RailwayViewWith(
+                railwayLength: RailwayLength.Create(1000.123456M, 800.654321M));
+
+            view.Should().NotBeNull();
+            view.TotalLength.Should().NotBeNull();
+            view.TotalLength.Kilometers.Should().Be(1000M);
+            view.TotalLength.Miles.Should().Be(801M);
+        }
+
+        [Fact]
+        public void RailwayView_ShouldRenderARailway_WithTrackGauge()
+        {
+            var view = RailwayViewWith(
+                railwayGauge: RailwayGauge.Create(TrackGauge.Standard.ToString(), 56.49606M, 1435.000000M));
+
+            view.Should().NotBeNull();
+            view.TrackGauge.Should().NotBeNull();
+            view.TrackGauge.Millimeters.Should().Be(1435.0M);
+            view.TrackGauge.Inches.Should().Be(56.5M);
+            view.TrackGauge.Gauge.Should().Be("Standard");
+        }
+
+        private static RailwayView RailwayViewWith(
+            Guid? id = null,
+            RailwayLength railwayLength = null,
+            RailwayGauge railwayGauge = null,
+            PeriodOfActivity periodOfActivity = null)
+        {
+            var railway = Railways.NewWith(
+                id: new RailwayId(id ?? new Guid("e8d33cd3-f36b-4622-90d1-76b450e0f313")),
+                name: "FS",
+                companyName: "Ferrovie dello stato",
+                slug: Slug.Of("FS"),
+                country: "IT",
+                //since: periodOfActivity?.OperatingSince,
+                //until: periodOfActivity?.OperatingUntil,
+                //status: periodOfActivity != null ? periodOfActivity.RailwayStatus : RailwayStatus.Active,
+                gauge: railwayGauge,
+                railwayLength: railwayLength,
+                website: new Uri("http://www.trenitalia.com"),
+                headquarters: "Roma");
+            return new RailwayView(railway, new LinksView());
+        }
+    }
+}
