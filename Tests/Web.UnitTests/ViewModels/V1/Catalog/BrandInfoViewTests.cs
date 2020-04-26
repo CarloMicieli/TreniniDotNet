ff733fd27c98191ce6045229e86c152949fbb57e
Carlo Micieli <carlo.micieli@gmail.com>
2020-04-26 10:53:07 +0200

    Added missing tests

---
+++ Tests/Web.UnitTests/ViewModels/V1/Catalog/BrandInfoViewTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using TreniniDotNet.TestHelpers.SeedData.Catalog;
+using Xunit;
+
+namespace TreniniDotNet.Web.ViewModels.V1.Catalog
+{
+    public class BrandInfoViewTests
+    {
+        [Fact]
+        public void BrandInfoView_ShouldCreateViewFromValues()
+        {
+            var acme = CatalogSeedData.Brands.Acme();
+            var view = new BrandInfoView(acme);
+
+            view.Should().NotBeNull();
+            view.Name.Should().Be(acme.Name);
+            view.Slug.Should().Be(acme.Slug);
+            view.Id.Should().Be(acme.BrandId.ToGuid());
+        }
+    }
+}
---
+++ Tests/Web.UnitTests/ViewModels/V1/Catalog/ScaleInfoViewTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using TreniniDotNet.TestHelpers.SeedData.Catalog;
+using Xunit;
+
+namespace TreniniDotNet.Web.ViewModels.V1.Catalog
+{
+    public class ScaleInfoViewTests
+    {
+        [Fact]
+        public void ScaleInfoView_ShouldReturnViewFromValues()
+        {
+            var H0 = CatalogSeedData.Scales.ScaleH0();
+
+            var view = new ScaleInfoView(H0);
+
+            view.Should().NotBeNull();
+            view.Slug.Should().Be(H0.Slug);
+            view.Name.Should().Be(H0.Name);
+            view.Id.Should().Be(H0.ScaleId.ToGuid());
+        }
+    }
+}
